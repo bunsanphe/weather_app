ceb68875b137b6ae8f82de20b2709d4d4987cee1
Heng <bunsanphe@gmail.com>
2020-05-09 15:38:50 -0700

    updates to scripts

--- script.js
+++ script.js
@@ -1,28 +1,115 @@
 $(document).ready(() => {
 
+var currentDate = moment().format("MM.DD.YYYY");
 
-// var apiKey = "eab6d01fa24f92ffa99be7e88ac10b4b"
-// queryURL = ("api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey);
-// fiveDayqueryURL = ("api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey);
 
 $("#submitBtn").on("click", function(){
     event.preventDefault();
     var city = $("#city").val();
-    console.log(city);
-
 
     var apiKey = "eab6d01fa24f92ffa99be7e88ac10b4b";
-    var queryURL = ("api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey);
-    var fiveDayqueryURL = ("api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey);
+    var queryURL = ("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey);
+    var fiveDayQueryURL = ("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey);
 
     $.ajax({url: queryURL, method: "GET"}).then(function(response){
-        console.log(response);
+        var city = response.name,
+            lonCoord = response.coord.lon,
+            latCoord = response.coord.lat,
+            iconcode = response.weather[0].icon,
+            temp = response.main.temp,
+            humidity = response.main.humidity,
+            windSpeed = response.wind.speed;
+            iconURL = ("http://openweathermap.org/img/w/" + iconcode + ".png");
+        localStorage.setItem("search", city)
+        cityResultBlock()
+        
+        $(".cityCard").text(city + " " + currentDate + " ")
+        $("#wicon").attr("src", iconURL);
+        $(".temp span").text(temp + "F");
+        $(".humidity span").text(humidity + "%");
+        $(".windSpeed span").text(windSpeed + "MPH")
+
+        //*uv results
+        var uvURL = ("http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + latCoord + "&lon=" + lonCoord);
+        $.ajax({url: uvURL, method: "GET"}).then(function(response){
+            var uvIndex = response.value;
+            var uvIndexBtnEl = $("<button></button>")
+            uvIndexBtnEl.text(uvIndex);
+            $(".uvIndex span").append(uvIndexBtnEl);
+        })
+
+
+        //*five day results
+        var fiveDayQueryURL = ("https://api.openweathermap.org/data/2.5/onecall?lat=" + latCoord + "&lon=" + lonCoord + "&units=imperial&exclude=hourly,current&appid=" + apiKey);
+        $.ajax({url: fiveDayQueryURL, method: "GET"}).then(function(response){
+            console.log(response)
+            // timeStamp = new Date((response.daily[i].dt)*1000);
+                var formattedDate1 = new Date((response.daily[1].dt)*1000);
+                var d = formattedDate1.getDate();
+                var m = formattedDate1.getMonth();
+                m +=1
+                var y = formattedDate1.getFullYear();
+                $("#dateDay_1").text(m + "." + d + "." + y)
+
+                var formattedDate2 = new Date((response.daily[2].dt)*1000);
+                var d = formattedDate2.getDate();
+                var m = formattedDate2.getMonth();
+                m +=1
+                var y = formattedDate2.getFullYear();
+                $("#dateDay_2").text(m + "." + d + "." + y)
+
+                var formattedDate3 = new Date((response.daily[3].dt)*1000);
+                var d = formattedDate3.getDate();
+                var m = formattedDate3.getMonth();
+                m +=1
+                var y = formattedDate3.getFullYear();
+                $("#dateDay_3").text(m + "." + d + "." + y)
+
+                var formattedDate4 = new Date((response.daily[4].dt)*1000);
+                var d = formattedDate4.getDate();
+                var m = formattedDate4.getMonth();
+                m +=1
+                var y = formattedDate4.getFullYear();
+                $("#dateDay_4").text(m + "." + d + "." + y)
+
+                var formattedDate5 = new Date((response.daily[5].dt)*1000);
+                var d = formattedDate5.getDate();
+                var m = formattedDate5.getMonth();
+                m +=1
+                var y = formattedDate5.getFullYear();
+                $("#dateDay_5").text(m + "." + d + "." + y)
+        
+            
+            //*icon five day
+            $(".iconFiveDay").each(function(i){
+                $(this).attr("src", ("http://openweathermap.org/img/w/" + response.daily[i].weather[0].icon + ".png"));
+            })
+
+            //*temp five day
+            $(".tempFiveDay").each(function(i){
+                $(this).text("Temp: " + response.daily[i].temp.day + " F");
+            })
+
+            //*humidity five day
+            $(".humidityFiveDay").each(function(i){
+                // var i = 1
+                $(this).text("Humidity: " + response.daily[i].humidity +"%");
+                console.log(this)
+            });
+            
+    
+        })
     })
 
 
-})
-//ajax url and get
+})//submit closing bracket
+
 function cityResultBlock(){
+    
+    var cityResult = localStorage.getItem("search");
+    console.log(cityResult)
+    var resultEl = $("<p></p>").text(cityResult);
+    $(".submit").append(resultEl);
     
 }
 //get the city val on click and combine qureryURL/API
@@ -32,46 +119,3 @@ function cityResultBlock(){
 
 
 })//ready closing bracket
-
-
-
-// **API RESPONE SHOULD SHOW 
-// {"coord": { "lon": 139,"lat": 35},
-//   "weather": [
-//     {
-//       "id": 800,
-//       "main": "Clear",
-//       "description": "clear sky",
-//       "icon": "01n"
-//     }
-//   ],
-//   "base": "stations",
-//   "main": {
-//     "temp": 281.52,
-//     "feels_like": 278.99,
-//     "temp_min": 280.15,
-//     "temp_max": 283.71,
-//     "pressure": 1016,
-//     "humidity": 93
-//   },
-//   "wind": {
-//     "speed": 0.47,
-//     "deg": 107.538
-//   },
-//   "clouds": {
-//     "all": 2
-//   },
-//   "dt": 1560350192,
-//   "sys": {
-//     "type": 3,
-//     "id": 2019346,
-//     "message": 0.0065,
-//     "country": "JP",
-//     "sunrise": 1560281377,
-//     "sunset": 1560333478
-//   },
-//   "timezone": 32400,
-//   "id": 1851632,
-//   "name": "Shuzenji",
-//   "cod": 200
-// }
